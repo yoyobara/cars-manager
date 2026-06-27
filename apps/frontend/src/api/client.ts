0dbf6b9e9b6b558f4c57bcb9a879034b41bcb34e
Yotam Barak <yotambarak6@gmail.com>
2026-06-28 00:38:27 +0300

    Bruh

--- apps/frontend/src/api/client.ts
+++ apps/frontend/src/api/client.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 console.log(API_BASE_URL)
+console.log(import.meta.env)
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
